b894d3d4d38ae8546749be77788dc271d34e66a7
barvit10 <barvit10@mail.ru>
2015-07-29 22:22:37 +0300

    first push

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link rel="stylesheet" href="/style/style.css" type="text/css" />
+</head>
+
+<body>
+
+<div class="wrapper">
+
+	<div class="header">
+		<strong></strong>
+	</div><!-- .header-->
+
+	<div class="middle">
+
+		<div class="container">
+			<div class="content">
+				<strong>Content:</strong>
+				
+				<table  cellspacing="5px" id="table_main" align='center'>
+				<tr>
+					<td align="center"><img src="/img/1.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/2.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/3.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/4.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/5.png" alt="альтернативный текст">Tp-Link</td>
+					</tr>
+				<tr>
+					<td align="center"><img src="/img/6.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/7.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/8.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/9.png" alt="альтернативный текст">Tp-Link</td>
+					<td align="center"><img src="/img/10.png" alt="альтернативный текст">Tp-Link</td>
+				</tr>
+				</table>
+				
+				
+				
+				
+			</div><!-- .content-->
+		</div><!-- .container-->
+
+		<div class="left-sidebar">
+			<strong><center>MENU</center>
+			<br>TP-LINK
+			<br>D-LINK
+			<br>Micro-Tik
+			</strong>
+	</div><!-- .left-sidebar -->
+
+	</div><!-- .middle-->
+
+	<div class="footer">
+		<strong>Footer:</strong>
+	</div><!-- .footer -->
+
+</div><!-- .wrapper -->
+
+</body>
+</html>
